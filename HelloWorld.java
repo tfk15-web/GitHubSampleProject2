4c92d0ee300d78a2f67b63b152db7edf351061b3
Tiabb Khan <khan_tayab@hotmail.com>
2025-09-24 15:13:15 +0200

    Konfliktændring fra VSCode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from VSCode!");
+        System.out.println("Ændring fra VSCode, som skaber konflikt!");
     }
 }

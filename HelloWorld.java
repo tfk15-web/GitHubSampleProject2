7071714c11e0a4effd0e4b099f2f327831ca212c
Tiabb Khan <khan_tayab@hotmail.com>
2025-09-24 15:31:53 +0200

    Konfliktændring fra IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from IntelliJ!");
+        System.out.println("Ændring fra IntelliJ, som skaber konflikt!");
     }
 }
